1794bfc4ba01e2ab82bcbcb56875ba73a5f26dff
Sam Joseph <tansaku@gmail.com>
2014-07-01 10:19:16 +0100

    first commit

---
+++ app/server.rb
@@ -0,0 +1,6 @@
+require 'sinatra'
+require 'data_mapper'
+require_relative 'models/model_template'
+require_relative 'controllers/controller_template'
+require_relative 'helpers/helpers_template'
+enable :sessions
